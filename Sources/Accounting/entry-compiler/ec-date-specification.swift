69056f663ce67cc68cf9fc67cffd02feb0f8a40c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-08-15 14:11:53 +0200

    custom encoding

--- Sources/Accounting/entry-compiler/ec-date-specification.swift
+++ Sources/Accounting/entry-compiler/ec-date-specification.swift
@@ -42,4 +42,35 @@ public enum DateSpecification: Hashable, Codable, Sendable, Equatable {
     //     guard let d = cal.date(from: comps) else { throw EntryDateError.invalidComponents(comps) }
     //     return d
     // }
+
+    private enum CodingKeys: String, CodingKey { case absolute, infer }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .absolute(let date):
+            let iso = ISO8601DateFormatter()
+            try container.encode(iso.string(from: date))
+        case .infer(let day):
+            try container.encode(["infer": day])
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let s = try? container.decode(String.self) {
+            if let d = ISO8601DateFormatter().date(from: s) {
+                self = .absolute(d)
+                return
+            }
+        }
+        if let dict = try? container.decode([String: Int].self),
+           let day = dict["infer"] {
+            self = .infer(day: day)
+            return
+        }
+        throw DecodingError.dataCorruptedError(in: container,
+            debugDescription: "Invalid DateSpecification format")
+    }
+
 }
