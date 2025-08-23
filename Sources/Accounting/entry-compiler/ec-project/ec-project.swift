9ca8f542de4b8fbde832ab75ba3bbe0cd779727d
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-08-23 14:44:50 +0200

    indenting project

--- Sources/Accounting/entry-compiler/ec-project/ec-project.swift
+++ Sources/Accounting/entry-compiler/ec-project/ec-project.swift
@@ -10,11 +10,16 @@ public struct EntryCompilerProject: Sendable {
 
     public func url(_ base: Base) -> URL {
         switch base {
-        case .config:     return root.appendingPathComponent("config", isDirectory: true)
-        case .entries:    return root.appendingPathComponent("entries", isDirectory: true)
-        case .statements: return root.appendingPathComponent("statements", isDirectory: true)
-        case .transactions: return root.appendingPathComponent("transactions", isDirectory: true)
-        case .test:       return root.appendingPathComponent("test", isDirectory: true)
+        case .config:
+            return root.appendingPathComponent("config", isDirectory: true)
+        case .entries:    
+            return root.appendingPathComponent("entries", isDirectory: true)
+        case .statements: 
+            return root.appendingPathComponent("statements", isDirectory: true)
+        case .transactions: 
+            return root.appendingPathComponent("transactions", isDirectory: true)
+        case .test:
+            return root.appendingPathComponent("test", isDirectory: true)
         }
     }
 }
