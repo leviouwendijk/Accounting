1614f65f478a987f31c6d4d56c3f7d91aa73e9bf
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-08-24 11:28:22 +0200

    removed advance() in date block (entry)

--- Sources/Accounting/entry-compiler/ec-parsers/ec-parsing-protocol/extensions/ext-entries/ec-ext-entry.swift
+++ Sources/Accounting/entry-compiler/ec-parsers/ec-parsing-protocol/extensions/ext-entries/ec-ext-entry.swift
@@ -23,31 +23,6 @@ public extension EntryCompilerParsing {
                 entry.timezone = parsed.identifier
 
             case .keyword("date"):
-                advance()
-                // if case .keyword("infer") = current {
-                //     advance()
-                //     guard case let .number(n) = current else {
-                //         throw ParserError.unexpectedToken(current, expected: "number (day of month)", at: loc())
-                //     }
-                //     entry.date = .infer(day: (n as NSDecimalNumber).intValue)
-                //     advance()
-
-                // } else if current == .equals {
-                //     advance()
-                //     switch current {
-                //     case let .number(n):
-                //         entry.date = .absolute(Date(timeIntervalSince1970: (n as NSDecimalNumber).doubleValue)); advance()
-                //     case let .dateLiteral(text):
-                //         entry.date = .absolute(try parseDateLiteral(text, in: tz)); advance()
-                //     default:
-                //         throw ParserError.unexpectedToken(current, expected: "number or dateLiteral", at: loc())
-                //     }
-
-                // } else if current == .lBrace {
-                //     entry.date = .absolute(try parseDateBlock(tz: tz))
-                // } else {
-                //     throw ParserError.unexpectedToken(current, expected: "infer, '=', or '{'", at: loc())
-                // }
                 entry.date = try parseDateOrInfer(tz: tz, allowUnixEpoch: true)
 
             case .keyword("details"):
