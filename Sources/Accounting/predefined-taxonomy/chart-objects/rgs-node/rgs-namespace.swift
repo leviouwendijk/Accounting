07dc33736b2856be176506b695b3f15edfcfeb46
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-08-27 18:04:17 +0200

    id_code for xbrl only

--- Sources/Accounting/predefined-taxonomy/chart-objects/rgs-node/rgs-namespace.swift
+++ Sources/Accounting/predefined-taxonomy/chart-objects/rgs-node/rgs-namespace.swift
@@ -1,13 +1,16 @@
 import Foundation
 
 public struct RGSNameSpace: Hashable, Codable, Sendable { 
+    public let id_code: String // such as "id_BImva" or whatever XBRL creates in xml
     public let space: String
     public let localization: String 
     
     public init(
+        id_code: String,
         space: String,
         localization: String
     ) {
+        self.id_code = id_code
         self.space = space
         self.localization = localization
     }
