9506cc0acbf31abc74216fc83feee8d35266b876
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-03 14:41:29 +0100

    unnecessary call site signature change, undo

--- Sources/Accounting/implementation/entry-compiler/ec-compiler/ec-compile-driver.swift
+++ Sources/Accounting/implementation/entry-compiler/ec-compiler/ec-compile-driver.swift
@@ -166,7 +166,7 @@ public struct EntryCompileDriver {
 
 extension EntryCompileDriver {
     // async overload for parallel perf
-    public static func compile_parallel(
+    public static func compile(
         projectRoot: URL,
         setting: CompileDriveSetting = CompileDriveSetting(),
         verbose: Bool = false
