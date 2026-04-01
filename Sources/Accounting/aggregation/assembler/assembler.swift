e602d9c02db47e8fe91986373a062729f1ed0af5
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-04-01 23:47:18 +0200

    cleanups, refactor

--- Sources/Accounting/aggregation/assembler/assembler.swift
+++ Sources/Accounting/aggregation/assembler/assembler.swift
@@ -1,30 +1,3 @@
 import Foundation
 
 public enum RGSAssembler {}
-
-extension RGSAssembler {
-    @inline(__always)
-    public static func makeForcedSets(
-        index: RGSIndex,
-        cut: AssembleCut,
-        parentById: [Int: Int]
-    ) -> (forcedIds: Set<Int>, forcedChain: Set<Int>) {
-        let forcedIds = Set(
-            cut.includeCodes.compactMap { 
-                index.byIdentifier[$0] 
-            }
-        )
-
-        let forcedChain: Set<Int> = cut.includeIntermediates
-            ? Set(
-                forcedIds.flatMap { 
-                    chainToRoot($0, parentById: parentById) 
-                }
-            ) : forcedIds
-
-        return (
-            forcedIds,
-            forcedChain
-        )
-    }
-}
